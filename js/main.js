e5dce4ca461e0e1224013ec9396de193a7583cdd
Stanisław Szarapka <s.szarapka10@gmail.com>
2020-05-28 12:30:39 +0200

    add nav and burger animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
+const menu = () => {
 
-const showNav = () => {
-    nav.classList.toggle('on');
+
+    const hamburger = document.querySelector('.hamburger');
+    const navOff = document.querySelectorAll('.nav-off');
+    const firstLine = document.querySelector('.hamburger__line--first');
+    const secondLine = document.querySelector('.hamburger__line--second');
+    const thirdLine = document.querySelector('.hamburger__line--third');
+    const nav = document.querySelector('.nav');
+
+    const navigation = () => {
+
+
+
+        const showNav = () => {
+            nav.classList.toggle('on');
+        }
+        const burgerAnimation = () => {
+
+
+            firstLine.classList.toggle('on');
+            secondLine.classList.toggle('on');
+            thirdLine.classList.toggle('on');
+        }
+
+        showNav();
+        burgerAnimation();
+    }
+    navOff.forEach((item) => {
+        item.addEventListener('click', () => {
+            firstLine.classList.remove('on');
+            secondLine.classList.remove('on');
+            thirdLine.classList.remove('on');
+            nav.classList.remove('on');
+        })
+    })
+    hamburger.addEventListener('click', navigation);
 }
-hamburger.addEventListener('click', showNav);
+menu();
