1fe1a1acf9e286a5a6c1de78de9f9ef10d501fa1
Stanisław Szarapka <s.szarapka10@gmail.com>
2020-07-09 14:36:40 +0200

    general improvements

--- js/fix_bg-image.js
+++
@@ -1,9 +0,0 @@
-// var bg = $('.main-photo');
-
-// function resizeBackground() {
-//     bg.height($(window).height());
-
-// }
-// $(window).resize(resizeBackground);
-// resizeBackground();
-// console.log($(window).height());
